056e2ebe5bdd9c437ee3269cbccffb5c2bf891b1
Yang Liu <foamliu@yeah.net>
2018-07-24 14:15:03 +0800

    update

--- website/router.py
+++ website/router.py
@@ -1,16 +1,25 @@
 from app import app, photos
 from flask import render_template, request, redirect, url_for, abort
+import cv2 as cv
+from utils import predict
 
 logger = app.logger
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/upload_photo', methods=['GET', 'POST'])
 def upload_photo():
     if request.method == 'POST' and 'photo' in request.files:
         photo = photos.save(request.files['photo'])
+        image_name = 'Image Relative Path'
+        gray = cv.imread(image_name, 0)
+        out = predict(gray)
+        cv.imwrite('uploads/output.png', out)
+
         return redirect(url_for('show', name=photo))
 
     return render_template('index.html')
