3dfba90a8f6583cd56e1fd3781b55f67444b389f
WANG PeiSheng <115681033@qq.com>
2018-07-31 15:58:28 +0800

    remove the index.html & combine index with show
    1. remove the index.html
    2. add static/picture folder to store images

--- website/router.py
+++ website/router.py
@@ -7,7 +7,7 @@ logger = app.logger
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return redirect(url_for('show', name="b3.jpg"))
 
 
 @app.route('/upload_photo', methods=['GET', 'POST'])
@@ -18,7 +18,7 @@ def upload_photo():
         photo_url = 'uploads/' + photo_name
         gray = cv.imread(photo_url, 0)
         out = predict(gray)
-        cv.imwrite('static/output_' + photo_name, out)
+        cv.imwrite('static/pictures/output_' + photo_name, out)
 
         return redirect(url_for('show', name=photo_name))
 
@@ -30,5 +30,11 @@ def show(name):
     if name is None:
         abort(404)
     src_url = photos.url(name)
-    dst_url = '../static/output_' + name
+    dst_url = '../static/pictures/output_' + name
+
+    initial = ["b1.jpg", "b2.jpg", "b3.jpg", "b4.jpg"]
+    if name in initial:
+        src_url = '../static/pictures/' + name
+        dst_url = '../static/pictures/output_' + name
+
     return render_template('show.html', src_url=src_url, dst_url=dst_url)
